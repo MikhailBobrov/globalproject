067a9261022b4c6b5ae3d307290be9c3c5daac43
MikhailBobrov <mikhailurban13@yandex.ru>
2023-01-20 13:47:58 +0500

    babel babel/preset-env babel-plugin-i18next-extract A babel plugin that can extract keys in JSONv3 format атвоматически достает ключи

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,6 +8,26 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: ["@svgr/webpack"],
   };
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ["ru", "en", "de"],
+              keyAsDefaultValue: true,
+            },
+          ],
+        ],
+      },
+    },
+  };
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -46,5 +66,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  return [typescriptLoader, cssLoader, fileLoader, svgLoader];
+  return [babelLoader, typescriptLoader, cssLoader, fileLoader, svgLoader];
 }
